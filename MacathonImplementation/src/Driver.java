035a85129f1f99c9a13c375ed3fd24b91587d736
dletk <dle@macalester.edu>
2017-02-25 01:54:56 -0600

    Create Driver and Passenger

---
+++ MacathonImplementation/src/Driver.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * Created by DucLe on 2/25/17.
+ */
+public class Driver extends User {
+
+    public Driver(String destination, String origin, Date dateAndTime, double price, int numberOfSeat) {
+        super(destination, origin, dateAndTime, price, numberOfSeat);
+    }
+
+}
---
+++ MacathonImplementation/src/Passenger.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+/**
+ * Created by DucLe on 2/25/17.
+ */
+public class Passenger extends User {
+
+    public Passenger(String destination, String origin, Date dateAndTime, double price, int numberOfSeat) {
+        super(destination, origin, dateAndTime, price, numberOfSeat);
+    }
+}
