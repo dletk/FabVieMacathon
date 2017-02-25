8bfc81194fc1140d8d3a8d8d0eeaefa78f92808e
dletk <dle@macalester.edu>
2017-02-25 01:48:56 -0600

    Initial commit

---
+++ MacathonImplementation/src/User.java
@@ -0,0 +1,20 @@
+import java.util.Date;
+
+/**
+ * Created by DucLe on 2/25/17.
+ */
+public class User {
+    private String destination;
+    private String origin;
+    private Date dateAndTime;
+    private double price;
+    private int numberOfSeat;
+
+    public User(String destination, String origin, Date dateAndTime, double price, int numberOfSeat) {
+        this.destination = destination;
+        this.origin = origin;
+        this.dateAndTime = dateAndTime;
+        this.price = price;
+        this.numberOfSeat = numberOfSeat;
+    }
+}
